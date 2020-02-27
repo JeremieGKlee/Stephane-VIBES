e32977cbe7797fb3224c04cf1dbde4064b9e711b
JeremieGKlee <jeremie.gosgnack@gmail.com>
2020-02-27 21:00:38 +0100

    encore bcp de boulot pour le vibe mais vive la sncf

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,27 +1,45 @@
-var content = document.querySelector("#burger_content");
-var sidebarBody = document.querySelector("#burger_sidebar_body");
-var button = document.querySelector("#burger_button");
-var overlay = document.querySelector("#burger_overlay");
-var activatedClass = "burger_activated";
+/*script burger*/
+// var content = document.querySelector("#burger_content");
+// var sidebarBody = document.querySelector("#burger_sidebar_body");
+// var button = document.querySelector("#burger_button");
+// var overlay = document.querySelector("#burger_overlay");
+// var activatedClass = "burger_activated";
 
-sidebarBody.innerHTML = content.innerHTML;
+// sidebarBody.innerHTML = content.innerHTML;
 
-button.addEventListener("click", function(e) {
-    e.preventDefault();
+// button.addEventListener("click", function(e) {
+//     e.preventDefault();
 
-    this.parentNode.classList.add(activatedClass);
-});
+//     this.parentNode.classList.add(activatedClass);
+// });
 
-button.addEventListener("keydown", function(e) {
-   if (this.parentNode.classList.contains(activatedClass)){
+// button.addEventListener("keydown", function(e) {
+//    if (this.parentNode.classList.contains(activatedClass)){
 
 //    if (e.repeat === false && e.which === 27)   Pour eviter erreur sur appui long touche et 27 correspond aà la touche echapp
-       this.parentNode.classList.remove(activatedClass);
-   }
-});
+//        this.parentNode.classList.remove(activatedClass);
+//    }
+// });
 
-overlay.addEventListener("click", function(e) {
-    e.preventDefault();
+// overlay.addEventListener("click", function(e) {
+//     e.preventDefault();
 
-    this.parentNode.classList.remove(activatedClass);
-});
+//     this.parentNode.classList.remove(activatedClass);
+// });
+/*script burger*/
+/*script accordion*/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+/*script accordion*/
