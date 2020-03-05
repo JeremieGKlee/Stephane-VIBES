5157dde864dc97eadf357a704a9cbe27a9e9eed7
JeremieGKlee <jeremie.gosgnack@gmail.com>
2020-03-05 16:04:38 +0100

    encore un effort

--- 2nde-version/Scripts/script.js
+++ 2nde-version/Scripts/script.js
@@ -53,14 +53,42 @@ $(".accordion h4").on("click", function(){
   }
   
 })
+
+// Modal origin C3SCHOOL
+// // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() { 
+//   modal.style.display = "none";
+// }
 // Get the modal
+// Modal origin C3SCHOOL
+
+
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName("myImg");
+var imgs = document.getElementsByClassName("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
+for(let i=0; i<imgs.length;i++){
+    imgs[i].onclick = onclick_img_alone;   
+}
+function onclick_img_alone(){
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
