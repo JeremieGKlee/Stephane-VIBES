a908a1d8dea45d4577f476b531c9493d08622d1c
JeremieGKlee <jeremie.gosgnack@gmail.com>
2020-03-13 01:17:25 +0100

    vibes fini mais sans sticky nav

--- 2nde-version/Scripts/script.js
+++ 2nde-version/Scripts/script.js
@@ -1,33 +1,79 @@
-/*script burger*/
-// var content = document.querySelector("#burger_content");
-// var sidebarBody = document.querySelector("#burger_sidebar_body");
-// var button = document.querySelector("#burger_button");
-// var overlay = document.querySelector("#burger_overlay");
-// var activatedClass = "burger_activated";
+/* Début Header  */
+/* Début script burger*/
+var content = document.querySelector("#burger_content");
+var sidebarBody = document.querySelector("#burger_sidebar_body");
+var button = document.querySelector("#burger_button");
+var overlay = document.querySelector("#burger_overlay");
+var activatedClass = "burger_activated";
 
-// sidebarBody.innerHTML = content.innerHTML;
+sidebarBody.innerHTML = content.innerHTML;
 
-// button.addEventListener("click", function(e) {
-//     e.preventDefault();
+button.addEventListener("click", function(e) {
+    e.preventDefault();
 
-//     this.parentNode.classList.add(activatedClass);
-// });
+    this.parentNode.classList.add(activatedClass);
+});
 
-// button.addEventListener("keydown", function(e) {
-//    if (this.parentNode.classList.contains(activatedClass)){
+button.addEventListener("keydown", function(e) {
+   if (this.parentNode.classList.contains(activatedClass)){
 
-//    if (e.repeat === false && e.which === 27)  /* Pour eviter erreur sur appui long touche et 27 correspond aà la touche echapp*/
-//        this.parentNode.classList.remove(activatedClass);
-//    }
-// });
+//    if (e.repeat === false && e.which === 27)   Pour eviter erreur sur appui long touche et 27 correspond aà la touche echapp
+       this.parentNode.classList.remove(activatedClass);
+   }
+});
 
-// overlay.addEventListener("click", function(e) {
-//     e.preventDefault();
+overlay.addEventListener("click", function(e) {
+    e.preventDefault();
 
-//     this.parentNode.classList.remove(activatedClass);
-// });
-/*script burger*/
-/*script accordion*/
+    this.parentNode.classList.remove(activatedClass);
+});
+/*Fin script burger*/
+/* Fin Header  */
+
+/* Début Goutte  */
+$("#goutte").on("click",function(){//ceci permet de simuler le click sur l'element cache input color
+  $("#drop_color").click()
+})
+
+$("#drop_color").on("change",changeDeCouleur);//sur l'action de cet element appelle la fonction change de couleur
+
+function changeDeCouleur(){
+  let nouvelle_couleur = $("#drop_color").val();   //
+  let skip_colors = document.getElementsByClassName("color");
+  for(let i=0; i<skip_colors.length;i++){
+  skip_colors[i].style.color = nouvelle_couleur;
+  }
+
+  let skip_back_colors = document.getElementsByClassName("backg_color");
+  for(let i=0; i<skip_back_colors.length;i++){
+  skip_back_colors[i].style.background = nouvelle_couleur;
+  }
+   
+  let skip_border_bot_colors = document.getElementsByClassName("bor_bot_col");
+  for(let i=0; i<skip_border_bot_colors.length;i++){
+  skip_border_bot_colors[i].style.borderBottomColor = nouvelle_couleur;
+  }
+  let skip_border_colors = document.getElementsByClassName("bor_col");
+  for(let i=0; i< skip_border_colors.length;i++){
+  skip_border_colors[i].style.borderColor = nouvelle_couleur;
+  }
+}
+document.addEventListener("DOMContentLoaded",function(event){  /* lorsque le DOM est chargé, on execute une fonction*/
+  let triahover = document.getElementById("tri").childNodes; /* je declare une variable let = qui cible les enfants d'un id particulier*/
+  for(let i=0;i<triahover.length;i++){          // boucle sur chacun des enfants
+  triahover[i].onmouseover=function(){          //quand je mets mon curseur dessus    
+    let nouvelle_couleur = $("#drop_color").val();    // je prends la valeur de mon input color
+    this.style.color = nouvelle_couleur;      // a celui là je lui donne la couleur de l'input
+    }
+    triahover[i].onmouseleave=function(){     // qd mon curseur sors de l'element, j'appelle une fonction anonyme qui redonne a l"element quitté la couleur d'origine
+    this.style.color = "#000";
+    }
+  }
+})
+/* Fin Goutte  */
+
+/*Debut accordion*/
+/*script accordion origin W3SCHOOL*/
 // var acc = document.getElementsByClassName("accordion");
 // var i;
 
@@ -42,7 +88,8 @@
 //     } 
 //   });
 // }
-/*script accordion*/
+/*script accordion origin W3SCHOOL*/
+/*script accordion origin VIRGIL*/
 $(".accordion h4").on("click", function(){
            
   if($(this).find("span").hasClass("fa-plus")){ 
@@ -51,10 +98,13 @@ $(".accordion h4").on("click", function(){
       $(this).find("span").addClass("fa-minus").removeClass("fa-plus")
       $(this).siblings("p").slideDown(400)
   }
-  
 })
+/*script accordion origin VIRGIL*/
+/*Fin accordion*/
 
-// Modal origin C3SCHOOL
+
+/*Début Modal*/
+// Modal origin W3SCHOOL
 // // Get the modal
 // var modal = document.getElementById("myModal");
 
@@ -98,53 +148,18 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
-//  Fin Modal
+/*Fin Modal*/
 
-$("#goutte").on("click",function(){
-  $("#drop_color").click()
-})
-
-
-$("#drop_color").on("change",changeDeCouleur);
-
-function changeDeCouleur(){
-  let nouvelle_couleur = $("#drop_color").val();   //
-  let skip_colors = document.getElementsByClassName("color");
-  for(let i=0; i<skip_colors.length;i++){
-  skip_colors[i].style.color = nouvelle_couleur;
-  }
-
-   let skip_back_colors = document.getElementsByClassName("backg_color");
-   for(let i=0; i<skip_back_colors.length;i++){
-    skip_back_colors[i].style.background = nouvelle_couleur;
-   }
-   
-   let skip_border_colors = document.getElementsByClassName("bor_col");
-   for(let i=0; i<skip_back_colors.length;i++){
-    skip_border_colors[i].style.borderBottomColor = nouvelle_couleur;
-   }
-}
-document.addEventListener("DOMContentLoaded",function(event){  /* lorsque le DOM est chargé, on execute une fonction*/
-  let triahover = document.getElementById("tri").childNodes; /* je declare une variable let = qui cible les enfants d'un id particulier*/
-  for(let i=0;i<triahover.length;i++){          // boucle sur chacun des enfants
-    triahover[i].onmouseover=function(){          //quand je mets mon curseur dessus    
-      let nouvelle_couleur = $("#drop_color").val();    // je prends la valeur de mon input color
-      this.style.color = nouvelle_couleur;      // a celui là je lui donne la couleur de l'input
-    }
-    triahover[i].onmouseleave=function(){     // qd mon curseur sors de l'element, j'appelle une fonction anonyme qui redonne a l"element quitté la couleur d'origine
-      this.style.color = "#000";
-    }
-  }
-})
-function filterSelection(tri){
-  let les_imgs = document.getElementById("portfolio").querySelectorAll("figure");
-    for (let i=0; i<les_imgs.length;i++){
-      if(!les_imgs[i].classList.contains(tri.toLowerCase())){
-        les_imgs[i].style.display="none";
+/*Début Filtre Portfolio*/
+function filterSelection(tri){ //cette function permet de selectionner sur filter selectionner le champs all dans exemple : a class="bold" onclick="filterSelection('All')">ALL</a>
+  let les_imgs = document.getElementById("portfolio").querySelectorAll("figure"); //tous les documents enfants de l'ID portfolio au nom de figure
+    for (let i=0; i<les_imgs.length;i++){ // tourne en boucle sur chaque enfants du tableau imgs
+      if(!les_imgs[i].classList.contains(tri.toLowerCase())){ //si sur chaque boucle imgs le contenu de la majuscule de tri est different de la selection 
+        les_imgs[i].style.display="none"; // alors tu caches ses enfants là
       }
-      else{
-        les_imgs[i].style.display="block";
+      else{//sinon
+        les_imgs[i].style.display="block"; //tu les montres
       }
-    }
-      
+    }  
 }
+/*Début Filtre Portfolio*/
